4ef5a04bb5d212e33f873800384a7806459b2f21
Roman Dyakov <dyakovri@gmail.com>
2021-11-12 01:30:13 +0300

    About layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,41 @@
 <template>
-  <div>
-    <p>About</p>
+  <div class="container-fluid">
+    <div>
+      <h2>Спасибо, что проявили интерес к отчету профкома</h2>
+      <p>
+        Сейчас вам будет предложено ответить на 6 вопросов о работе профкома по
+        следующей повестке:
+      </p>
+      <ol>
+        <li>
+          Отчет Профкома о работе за период ноябрь 2020 г. - октябрь 2021 г.
+        </li>
+        <li>
+          Отчет контрольно-ревизионной комиссии за период ноябрь 2020 г. -
+          октябрь 2021 г.
+        </li>
+        <li>Выборы членов Профкома студентов.</li>
+        <li>Выборы контрольно-ревизионной комиссии Профкома студентов.</li>
+        <li>Выборы контрольно-ревизионной комиссии Профкома студентов.</li>
+        <li>Разное.</li>
+      </ol>
+    </div>
+    <div class='controls'>
+      <button class="btn btn-primary">Начать!</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
+
+<style scoped>
+div.container-fluid > * {
+  text-align: left;
+}
+
+div.controls {
+  text-align: center;
+}
+</style>
