c5ff1ca4617c6499bd086f86945822536e5e1ad4
Roman Dyakov <dyakovri@gmail.com>
2021-11-12 12:02:44 +0300

    Fail dialog

--- src/views/Fail.vue
+++ src/views/Fail.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>
-    <p>Fail</p>
+  <div class="dialog-container">
+    <h2>Не удалось</h2>
+    <p>
+      Что-то пошло не так при входе. Возможно вы неверно ввели адрес электронной
+      почты или пароль. Попробуйте еще раз.
+      <a
+        href="https://report.profcomff.com/faq"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Не помогло?
+      </a>
+    </p>
+    <button class="btn btn-primary">Вернуться</button>
   </div>
 </template>
 
